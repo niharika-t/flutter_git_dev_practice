6440c55c2ffa1d65e0342df53dd445f7b8f7280d
Niharika <tiwarinst6@gmail.com>
2023-07-03 15:13:44 +0530

    added : popup window when click on image

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -18,12 +18,33 @@ class _HomeScreenState extends State<HomeScreen> {
           title: const Text("GitPractice"),
         ),
         body:  Center(
-          child: Column(
-           children: <Widget>[
-             Text("Niharika Shivprakash Tiwari"),
-             Image.asset("assets/images/codespace.png"),
-           ]
-          ),
+          child: GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Image Popup'),
+                      content: Text('This is a popup window.'),
+                      actions: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Close'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Column(
+               children: <Widget>[
+                 Text("Niharika Shivprakash Tiwari"),
+                 Image.asset("assets/images/codespace.png"),
+               ]
+              ),
+         ),
         ),
       ),
     );
